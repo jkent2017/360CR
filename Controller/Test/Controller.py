50877595e5d87e5fc268ba2c01d3b899d192cffa
Jordan Kent <jkent2017@my.fit.edu>
2020-03-09 16:32:35 -0400

    Added max speed control

--- Controller/Test/Controller.py
+++ Controller/Test/Controller.py
@@ -16,6 +16,7 @@ driveChar = 0
 turnChar = 0
 digital1 = 1
 digital2 = 5
+maxSpeed = 5.0
 joysticks = []
 buttonString = "Temp"
 
@@ -31,12 +32,12 @@ while not gameExit:
   if (driveChar < deadzone) & (driveChar > -1*deadzone):
     driveChar = 0;
   else:
-    driveChar = int(-127 * driveChar)
+    driveChar = int(-127 * maxSpeed/5.0 * driveChar)
 
   if (turnChar < deadzone) & (turnChar > -1*deadzone):
     turnChar = 0;
   else:
-    turnChar = int(127 * turnChar)
+    turnChar = int(127 * maxSpeed/5.0 * turnChar)
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -44,9 +45,13 @@ while not gameExit:
     elif event.type == pygame.JOYBUTTONDOWN:
       if event.button == 0:
         buttonString = "D-Pad Up"
+        if maxSpeed < 5:
+        	maxSpeed = maxSpeed + 1
         pass # D-Up
       elif event.button == 1:
         buttonString = "D-Pad Down"
+        if maxSpeed > 1:
+        	maxSpeed = maxSpeed - 1
         pass # D-Down
       elif event.button == 2:
         buttonString = "D-Pad Left"
@@ -93,7 +98,8 @@ while not gameExit:
         pass # Y
       print("Button Pressed: " + buttonString)
   ser.write(str(sync1) + " " + str(sync2) + " " + str(sync3) + " " + str(driveChar) + " " + str(turnChar) + " " + str(digital1) + " " + str(digital2) + "\n")
-  print(str(sync1) + " " + str(sync2) + " " + str(sync3) + " " + str(driveChar) + " " + str(turnChar) + " " + str(digital1) + " " + str(digital2) + "\n")
+  print(str(sync1) + " " + str(sync2) + " " + str(sync3) + " " + str(driveChar) + " " + str(turnChar) + " " + str(digital1) + " " + str(digital2))
+  print("Max Speed: " + str(maxSpeed) + "\n")
   time.sleep(0.05)
 ser.close()
 pygame.quit()
